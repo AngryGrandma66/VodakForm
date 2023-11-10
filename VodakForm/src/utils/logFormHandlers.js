28c32d0e1a87875aa966042d19ce7bde5edc1022
AngryGrandma <95356846+AngryGrandma66@users.noreply.github.com>
2023-11-10 11:26:41 +0100

    Update logFormHandlers.js

--- VodakForm/src/utils/logFormHandlers.js
+++ VodakForm/src/utils/logFormHandlers.js
@@ -14,28 +14,24 @@ async function submitData(formData) {
             body: JSON.stringify(formData)
         });
 
-        return await response.json(); // or handle the response as needed
+        return await response.json(); 
     } catch (error) {
         console.error('Submission error:', error);
         alert('Došlo k chybě.');
     }
 }
 
-// Adjusted function to handle the form submission
 export async function logSubmitForm() {
     const currentLoginUsername= get(loginUsername)
     const currentLoginPassword= get(loginPassword)
-    // Construct the form data object
     const formData = {
         loginUsername: currentLoginUsername.trim().toLowerCase(),
         loginPassword: currentLoginPassword
     };
-    // Submit the form data
     const result = await submitData(formData);
 
     if (result?.success) {
         alert('Login byl úspěšný');
-        // Optionally reset the form or redirect the user
         window.location.href = '/'
     } else {
         alert('Jméno nebo heslo není správně');
