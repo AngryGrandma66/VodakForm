475a25d45d6f2bd6da7003460b76db5e75936092
AngryGrandma <95356846+AngryGrandma66@users.noreply.github.com>
2023-11-10 11:23:35 +0100

    Update +server.js

--- VodakForm/src/routes/logout/+server.js
+++ VodakForm/src/routes/logout/+server.js
@@ -2,26 +2,22 @@ import { endSession } from '$lib/sessionStore';
 import { serialize } from 'cookie';
 
 export async function GET({ locals }) {
-    // Terminate the session in your session storage
     await endSession(locals.user?.sessionId);
 
-    // Clear the session cookie
     const headers = {
         'Set-Cookie': serialize('session_id', '', {
             path: '/',
-            httpOnly: true, // Cookie is not accessible via JavaScript
-            expires: new Date(0), // set the cookie to expire immediately
-            // SameSite can be set to strict for additional security
+            httpOnly: true,
+            expires: new Date(0), 
             sameSite: 'strict',
         }),
     };
 
-    // Redirect to home or login page after logout
     return new Response(null, {
-        status: 303, // Status code 303 means "See Other" (a redirect with GET method)
+        status: 303, 
         headers: {
             ...headers,
-            'Location': '/', // or '/' for redirecting to the home page
+            'Location': '/', 
         }
     });
 }
